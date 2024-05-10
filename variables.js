56497a6debe7cbde1020bca98640e0bc057f4553
ace <leetgrind.org@gmail.com>
2024-05-10 09:56:35 -0400

    added variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+// variables
+var className = "Web development"; // camel case
+console.log(className);
+
+className = "Web dev";
+
+console.log(className);
+
+// constants
+const constantName = 1;
+console.log(constantName);
+constantName = 10;
+console.log(constantName);
