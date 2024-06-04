cb4e4fd0de3441c580af90ec9b5ee6e4d0144ac6
leetgrind <leegrind.org@gmail.com>
2024-06-04 09:54:26 -0400

    added post

--- week3/controller/userController.js
+++ week3/controller/userController.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const userModel = require("../models/user");
 
 const router = express.Router();
+const jsonParser = bodyParser.json();
+
+router.post("/", jsonParser, async (req, res) => {
+
+    console.log(req.body)
+
+    const newUser = new userModel({
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password
+    });
+
+    try {
+        const newData = await newUser.save();
+        res.status(201).json(newData)
+    }
+    catch (err) {
+        res.status(500).json({message: err.message})
+    }
+});
+
+
 
 router.get("/", async (req, res) => {
     try{
